8340a4f6b3c7dac8d7b548f33f6b5c6439cd75db
Tommi tofthøj <ttofthoj@gmail.com>
2021-11-30 21:41:41 +0100

    reconfigure usb for linux

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import serial
 while 1:
     var = int(input("enter number: "))
     if var == 1:
-        ser = serial.Serial('dev/tty/')
+        ser = serial.Serial('/dev/ttyUSB0')
         print(ser.name)
     else:
-        ser = serial.Serial('COM7')
+        ser = serial.Serial('/dev/ttyUSB1')
         print(ser.name)
     xd = str(input("enter a number: ")).encode('utf-8')
     ser.write(xd + b'\n')
