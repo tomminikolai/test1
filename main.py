6063271910953b0f5c66ceacb08a40b6cea3d601
Tommi tofthøj <ttofthoj@gmail.com>
2021-12-01 11:18:17 +0100

    remove try

--- main.py
+++ main.py
@@ -3,16 +3,13 @@ import serial
 while 1:
     var = int(input("enter number: "))
     if var == 1:
-        ser = serial.Serial('/dev/ttyUSB0')
+        ser = serial.Serial('/dev/ttyUSB0', timeout=1)
         print(ser.name)
     else:
         ser = serial.Serial('/dev/ttyUSB1')
         print(ser.name)
     xd = str(input("enter a number: ")).encode('utf-8')
-    try:
-        ser.write(xd + b'\n')
-    except:
-        print("couldn't connect")
+    ser.write(xd + b'\n')
     if int(xd.decode('utf-8')) == 1:
         line = ser.readline().decode('utf-8').rstrip()
         print(line)
