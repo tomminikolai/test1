37f48d51cb5d05578e52c4acda58374224ea7d9c
Tommi tofthøj <ttofthoj@gmail.com>
2021-12-01 10:53:12 +0100

    somewhat fix colour sensor issue

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 import serial
 while 1:
     var = int(input("enter number: "))
@@ -9,6 +10,7 @@ while 1:
         print(ser.name)
     xd = str(input("enter a number: ")).encode('utf-8')
     ser.write(xd + b'\n')
-    line = ser.readline().decode('utf-8').rstrip()
-    print(line)
+    if int(xd.decode('utf-8')) == 1:
+        line = ser.readline().decode('utf-8').rstrip()
+        print(line)
     ser.close()
