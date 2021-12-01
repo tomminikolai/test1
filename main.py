6684a1167b5f7535cae3b4f653426c1e4f9ece77
Tommi tofthøj <ttofthoj@gmail.com>
2021-12-01 11:14:24 +0100

    add try

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ while 1:
         ser = serial.Serial('/dev/ttyUSB1')
         print(ser.name)
     xd = str(input("enter a number: ")).encode('utf-8')
-    ser.write(xd + b'\n')
+    try:
+        ser.write(xd + b'\n')
+    except:
+        print("couldn't connect")
     if int(xd.decode('utf-8')) == 1:
         line = ser.readline().decode('utf-8').rstrip()
         print(line)
