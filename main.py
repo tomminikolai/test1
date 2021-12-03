ea2a43a858972cca37d06459f789f73ef4f46dbb
Tommi tofthøj <ttofthoj@gmail.com>
2021-12-03 14:20:54 +0100

    fix print issue

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import time
 import serial
+ser1 = serial.Serial('/dev/ttyUSB0')
+print(ser1.name)
+ser2 = serial.Serial('/dev/ttyUSB1')
+print(ser2.name)
 while 1:
     var = int(input("enter number: "))
-    if var == 1:
-        ser = serial.Serial('/dev/ttyUSB0', timeout=1)
-        print(ser.name)
-    else:
-        ser = serial.Serial('/dev/ttyUSB1')
-        print(ser.name)
     xd = str(input("enter a number: ")).encode('utf-8')
-    ser.write(xd + b'\n')
-    if int(xd.decode('utf-8')) == 1:
-        line = ser.readline().decode('utf-8').rstrip()
-        print(line)
-    ser.close()
+    if var == 1:
+        ser1.write(xd + b'\n')
+        if int(xd.decode('utf-8')) == 1:
+            line = ser1.readline().decode('utf-8').rstrip()
+    else:
+        ser2.write(xd + b'\n')
+        if int(xd.decode('utf-8')) == 1:
+            line = ser2.readline().decode('utf-8').rstrip()
+    print(line)
