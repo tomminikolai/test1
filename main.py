e7fb1a264dc3313fbbbd446c7cb75e52b70fc7ca
Tommi tofthøj <ttofthoj@gmail.com>
2021-12-16 13:07:31 +0100

    add selective amounts of usb devices

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import serial
-ser1 = serial.Serial('/dev/ttyUSB0')
-print(ser1.name)
-ser2 = serial.Serial('/dev/ttyUSB1')
-print(ser2.name)
+lmao = int(input("enter number: "))
+if lmao > 0:
+    ser1 = serial.Serial('/dev/ttyUSB0')
+    print(ser1.name)
+if lmao > 1:
+    ser2 = serial.Serial('/dev/ttyUSB1')
+    print(ser2.name)
 while 1:
     var = int(input("enter number: "))
     xd = str(input("enter a number: ")).encode('utf-8')
